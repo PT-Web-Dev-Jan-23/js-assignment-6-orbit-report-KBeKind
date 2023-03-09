ed213bbd0605c4e896acce67b510f74b2bbc4435
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-03-09 15:30:56 -0600

    finished bonus added green to every other row and made it searchable for name or type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -42,7 +42,10 @@ export class AppComponent {
 		searchTerm = searchTerm.toLowerCase();
 		for(let i=0; i < this.sourceList.length; i++) {
 			let name = this.sourceList[i].name.toLowerCase();
+			let type = this.sourceList[i].type.toLowerCase();
 			if (name.indexOf(searchTerm) >= 0) {
+				matchingSatellites.push(this.sourceList[i]);
+			} else if (type.indexOf(searchTerm) >= 0) {
 				matchingSatellites.push(this.sourceList[i]);
 			}
 		}
